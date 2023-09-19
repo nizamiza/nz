2ec7f467dd99f4fe07f9033471c3afceb25eff5d
Niza Toshpulatov <niza@hey.com>
2023-09-19 07:47:05 +0200

    Add core

---
+++ README.md
@@ -0,0 +1,7 @@
+# NZ
+
+NZ - is a simple helper framework for working with Web Components.
+
+## Usage
+
+TBD.
---
+++ lib/global.d.ts
@@ -0,0 +1,66 @@
+type KebabCase<Value extends string> = Value extends `${infer A}${infer B}`
+  ? A extends "-" // Check if the first character is a hyphen
+    ? B extends `-${string}` // Check if the next character is also a hyphen
+      ? "Invalid" // If consecutive hyphens found, it's invalid
+      : KebabCase<B> // Continue checking the rest of the string
+    : A extends `${Lowercase<A>}${string}` // Check if the first character is lowercase
+    ? KebabCase<B> // Continue checking the rest of the string
+    : "Invalid" // If an uppercase letter found, it's invalid
+  : "Valid"; // If the string has been fully checked, it's valid
+
+type KebabCasedString<Value> = Value extends string
+  ? KebabCase<Value> extends "Valid"
+    ? Value
+    : never
+  : never;
+
+interface BaseElement {
+  new (): HTMLElement;
+}
+
+type CustomElementRegistryDefinition = Parameters<
+  CustomElementRegistry["define"]
+>;
+
+type SlotMap = { default: Node[] } & {
+  [Key in string]: Node[];
+};
+
+type CustomElementInitArgs = {
+  shadowRoot: ShadowRoot;
+  slots: SlotMap;
+};
+
+type CustomElementDefinition<Value extends string = string> = {
+  /**
+   * Name of the custom element. Must be in kebab-case (dash separated).
+   */
+  name: KebabCasedString<Value>;
+  /**
+   * Base class used for the class of the custom element. Defaults to
+   * HTMLElement.
+   */
+  baseElement?: BaseElement;
+  options?: ElementDefinitionOptions;
+  /**
+   * Module import url (`import.meta.url`). Used for resolving template file.
+   */
+  moduleUrl: string;
+  /**
+   * Init function called inside of the custom element's constructor.
+   */
+  init?: (this: HTMLElement, args: CustomElementInitArgs) => void;
+  attributeMap?: {
+    [CssQuerySelector in string]?: {
+      [AttributeName in string]?:
+        | string
+        | boolean
+        | ((this: HTMLElement, attr: Attr | null) => string);
+    };
+  };
+  adoptedStyleSheets?: CSSStyleSheet[];
+};
+
+type InitCustomElement = (
+  definition: CustomElementDefinition,
+) => Promise<CustomElementRegistryDefinition>;
