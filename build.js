fc856737a37018875a1fc3e1a4a7ae584017d4d3
Niza Toshpulatov <niza@hey.com>
2023-10-15 18:01:54 +0200

    Add fallback templates

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 import { exec as unpromisifiedExec } from "child_process";
 import { promisify } from "util";
-import { copyFile, rm, writeFile } from "fs/promises";
+import { copyFile, mkdir, rm, writeFile } from "fs/promises";
 import packageJson from "./package.json" assert { type: "json" };
 
 const exec = promisify(unpromisifiedExec);
@@ -28,4 +28,18 @@ await writeFile(
 
 await copyFile("./README.md", "./dist/README.md");
 await copyFile("./lib/index.d.ts", "./dist/index.d.ts");
+
 await copyFile("./lib/nz-head/nz-head.html", "./dist/lib/nz-head/nz-head.html");
+
+await mkdir("./dist/lib/core/templates", {
+  recursive: true,
+});
+
+await copyFile(
+  "./lib/core/templates/loading.html",
+  "./dist/lib/core/templates/loading.html",
+);
+await copyFile(
+  "./lib/core/templates/bootstrap.html",
+  "./dist/lib/core/templates/bootstrap.html",
+);
